eecb85e28ec647f346659037e2f7a266f2fe70e0
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2023-04-24 13:03:55 +0300

    Выполнено

--- lvl_1/task_1.1.py
+++ lvl_1/task_1.1.py
@@ -13,3 +13,33 @@ print(my_favorite_songs[0:13])
 print(my_favorite_songs[64:])
 print(my_favorite_songs[16:30])
 print(my_favorite_songs[-61:-47])
+
+# Да, но можно решить задачку, используя встроенные методы строк
+# Например
+# Решение с помощью индексации строк
+
+first_song = my_favorite_songs [
+    : my_favorite_songs.find(',')
+]
+
+last_song = my_favorite_songs [
+    my_favorite_songs.rfind('N') :
+]
+
+second_song = my_favorite_songs[
+     my_favorite_songs.find('S') : 
+     my_favorite_songs.find(', A')
+]
+
+previous_song = my_favorite_songs [
+    my_favorite_songs.rfind('St') : 
+    my_favorite_songs.rfind(', N')
+    ]
+
+print(first_song, last_song, second_song, previous_song)
+
+
+# Решение с помощью метода split() и индексации списков
+songs = my_favorite_songs.split(', ')
+
+print(songs[0], songs[-1], songs[1], songs[-2])
